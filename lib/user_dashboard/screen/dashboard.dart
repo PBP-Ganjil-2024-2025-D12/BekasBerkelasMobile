0e8d6b916fce8d500e574566117a22f656ce2dc8
Muhammad Naufal Ramadhan <naufalnauf04@gmail.com>
2024-12-11 00:50:59 +0700

    fetch user data laman dashboard

--- lib/user_dashboard/screen/dashboard.dart
+++ lib/user_dashboard/screen/dashboard.dart
@@ -1,216 +1,215 @@
+import 'dart:convert';
 import 'package:bekas_berkelas_mobile/widgets/left_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'change_name.dart';
 import 'change_email.dart';
 import 'change_NoTelp.dart';
 import 'change_password.dart';
 
-class DashboardPage extends StatelessWidget {
-  final String name = "Tes";
-  final String email = "Tes@gmail.com";
-  final String phoneNumber = "+12345";
-  final String imageUrl = "";
+class DashboardPage extends StatefulWidget {
+  @override
+  _DashboardPageState createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  String name = '';
+  String email = '';
+  String phoneNumber = '';
+  String imageUrl = '';
+
+  Future<Map<String, dynamic>> fetchData(CookieRequest request) async {
+    try {
+      final response = await request.post('http://localhost:8000/dashboard/get_user_flutter/', {});
+      if (response['status'] == 'success') {
+        return response;  
+      } else {
+        throw Exception('Failed to load data: ${response['status']}');
+      }
+    } catch (e) {
+      throw Exception('Failed to parse JSON: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
     return Scaffold(
       appBar: AppBar(
         title: Text('Dashboard'),
       ),
       drawer: const LeftDrawer(),
-      body: Column(
-        children: [
-          Expanded(
-            flex: 3,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Center(
-                    child: CircleAvatar(
-                      radius: 50,
-                      backgroundImage: NetworkImage(imageUrl),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Center(
-                    child: SizedBox(
-                      width: 300,
-                      child: Row(
-                        children: [
-                          const Text(
-                            'Name',
-                            style: TextStyle(fontSize: 18, color: Colors.grey),
-                          ),
-                          const SizedBox(width: 10),
-                          Text(
-                            ': $name',
-                            style: const TextStyle(fontSize: 18, color: Colors.black),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  Center(
-                    child: SizedBox(
-                      width: 300,
-                      child: Row(
-                        children: [
-                          const Text(
-                            'Email',
-                            style: TextStyle(fontSize: 18, color: Colors.grey),
-                          ),
-                          const SizedBox(width: 10),
-                          Text(
-                            ': $email',
-                            style: const TextStyle(fontSize: 18, color: Colors.black),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  Center(
-                    child: SizedBox(
-                      width: 300,
-                      child: Row(
-                        children: [
-                          const Text(
-                            'Phone Number',
-                            style: TextStyle(fontSize: 18, color: Colors.grey),
-                          ),
-                          SizedBox(width: 10),
-                          Text(
-                            ': $phoneNumber',
-                            style: TextStyle(fontSize: 18, color: Colors.black),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 40),
-                  Center(
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: fetchData(request),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot}'));
+          } else if (snapshot.hasData) {
+            final data = snapshot.data!;
+            name = data['nama'];
+            email = data['email'];
+            phoneNumber = data['no_telp'];
+            imageUrl = data['profile_picture'] ?? '';
+
+            return Column(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        ElevatedButton.icon(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => ChangeNamePage()),
-                            );
-                          },
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: Size(double.infinity, 50),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
+                        Center(
+                          child: CircleAvatar(
+                            radius: 50,
+                            backgroundImage: NetworkImage(imageUrl),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Center(
+                          child: SizedBox(
+                            width: 300,
+                            child: Row(
+                              children: [
+                                const Text(
+                                  'Name',
+                                  style: TextStyle(fontSize: 18, color: Colors.grey),
+                                ),
+                                const SizedBox(width: 10),
+                                Text(
+                                  ': $name',
+                                  style: const TextStyle(fontSize: 18, color: Colors.black),
+                                ),
+                              ],
                             ),
                           ),
-                          icon: Icon(Icons.person),
-                          label: Text('Ubah Nama'),
                         ),
                         const SizedBox(height: 10),
-                        ElevatedButton.icon(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => ChangeEmailPage()),
-                            );
-                          },
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: Size(double.infinity, 50),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
+                        Center(
+                          child: SizedBox(
+                            width: 300,
+                            child: Row(
+                              children: [
+                                const Text(
+                                  'Email',
+                                  style: TextStyle(fontSize: 18, color: Colors.grey),
+                                ),
+                                const SizedBox(width: 10),
+                                Text(
+                                  ': $email',
+                                  style: const TextStyle(fontSize: 18, color: Colors.black),
+                                ),
+                              ],
                             ),
                           ),
-                          icon: Icon(Icons.email),
-                          label: Text('Ubah Email'),
                         ),
                         const SizedBox(height: 10),
-                        ElevatedButton.icon(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => ChangePhonePage()),
-                            );
-                          },
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: Size(double.infinity, 50),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
+                        Center(
+                          child: SizedBox(
+                            width: 300,
+                            child: Row(
+                              children: [
+                                const Text(
+                                  'Phone Number',
+                                  style: TextStyle(fontSize: 18, color: Colors.grey),
+                                ),
+                                SizedBox(width: 10),
+                                Text(
+                                  ': $phoneNumber',
+                                  style: TextStyle(fontSize: 18, color: Colors.black),
+                                ),
+                              ],
                             ),
                           ),
-                          icon: Icon(Icons.phone),
-                          label: Text('Ubah No Telp'),
                         ),
-                        const SizedBox(height: 10),
-                        ElevatedButton.icon(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => ChangePasswordPage()),
-                            );
-                          },
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: Size(double.infinity, 50),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
+                        const SizedBox(height: 40),
+                        Center(
+                          child: Column(
+                            children: [
+                              ElevatedButton.icon(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => ChangeNamePage()),
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: Size(double.infinity, 50),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                ),
+                                icon: Icon(Icons.person),
+                                label: Text('Ubah Nama'),
+                              ),
+                              const SizedBox(height: 10),
+                              ElevatedButton.icon(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => ChangeEmailPage()),
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: Size(double.infinity, 50),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                ),
+                                icon: Icon(Icons.email),
+                                label: Text('Ubah Email'),
+                              ),
+                              const SizedBox(height: 10),
+                              ElevatedButton.icon(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => ChangePhonePage()),
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: Size(double.infinity, 50),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                ),
+                                icon: Icon(Icons.phone),
+                                label: Text('Ubah No Telp'),
+                              ),
+                              const SizedBox(height: 10),
+                              ElevatedButton.icon(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => ChangePasswordPage()),
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: Size(double.infinity, 50),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                ),
+                                icon: Icon(Icons.lock),
+                                label: Text('Ubah Password'),
+                              ),
+                            ],
                           ),
-                          icon: Icon(Icons.lock),
-                          label: Text('Ubah Password'),
                         ),
                       ],
                     ),
                   ),
-                ],
-              ),
-            ),
-          ),
-          const Divider(),
-          Expanded(
-            flex: 1,
-            child: Column(
-              children: [
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      // Navigasi ke halaman Dashboard
-                    },
-                    style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                      minimumSize: Size(double.infinity, double.infinity),
-                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                      alignment: Alignment.center,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                    ),
-                    child: Text('Dashboard'),
-                  ),
-                ),
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      // Navigasi ke halaman Home
-                    },
-                    style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                      minimumSize: Size(double.infinity, double.infinity),
-                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                      alignment: Alignment.center,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                    ),
-                    child: Text('Home'),
-                  ),
                 ),
               ],
-            ),
-          ),
-        ],
+            );
+          } else {
+            return Center(child: Text('No data available'));
+          }
+        },
       ),
     );
   }
