828c2224b406fdffe27df3dfab1b5d702db22239
widaputri <widapk2709@gmail.com>
2024-12-14 22:59:16 +0700

    Remove unused import in profile.dart

--- lib/review_rating/screens/profile.dart
+++ lib/review_rating/screens/profile.dart
@@ -2,7 +2,6 @@ import 'package:bekas_berkelas_mobile/review_rating/models/review_rating.dart';
 import 'package:bekas_berkelas_mobile/review_rating/models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
-import 'dart:convert';
 import 'package:bekas_berkelas_mobile/review_rating/widgets/review_card.dart';
 import 'package:bekas_berkelas_mobile/review_rating/screens/reviews_page.dart';
 
